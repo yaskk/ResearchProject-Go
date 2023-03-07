d945efd3e129ced4070440912662c961f3f4e2b6
epohl <epohl01@mynbcc.ca>
2023-03-07 16:46:45 -0400

    SQL statement for creating database, table, and inserting mock-up data

---
+++ CreateDbMySql.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS todo_list;
+USE todo_list;
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(45) NOT NULL,
+  description VARCHAR(150) NULL,
+  priority INT NOT NULL
+);
+
+INSERT INTO tasks (title, description, priority)
+VALUES ('Buy groceries', 'Milk, bread, and eggs', 1),
+       ('Finish project', 'Complete the report and submit it', 2),
+       ('Call John', NULL, 3),
+       ('Schedule appointment', 'Meet with the dentist on Friday', 2);
